352cf8c0f70e3ad50b6b7ba6655e3f751c2ba1fe
EgiglioUade <146669271+Zeki19@users.noreply.github.com>
2024-09-29 17:04:43 -0300

    Health.cs created for Decorator

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace HealthSystem
+{
+    public class Health
+    {
+        public int MaxHP { get; private set; }
+        public int CurrentHP { get; private set; }
+
+        public event Action OnDeath;
+
+        public Health(int maxHealth)
+        {
+            MaxHP = maxHealth;
+            CurrentHP = maxHealth;
+        }
+
+        public void TakeDamage(int damage)
+        {
+            CurrentHP -= damage;
+            if (CurrentHP <= 0 )
+            {
+                CurrentHP = 0;
+                OnDeath?.Invoke();
+            }
+        }
+
+        public int GetCurrentHealth()
+        {
+            return CurrentHP;
+        }
+    }
+}
